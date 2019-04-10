93bde21b30ddda5aa8c773b886ec74cbf2f8ae6c
applesaucyy <applesaucy@yandex.com>
2019-04-10 15:16:37 -0400

    all the frontend shit should be done now

--- src/acutetv.cr
+++ src/acutetv.cr
@@ -2,6 +2,7 @@ require "kemal"
 require "ecr"
 require "file_utils"
 require "markdown"
+require "kemal-basic-auth"
 
 def v_dur(v)
     return `ffmpeg -i #{v} 2>&1 | grep "Duration"| cut -d ' ' -f 4 | sed s/,// | sed 's@\..*@@g' | awk '{ split($1, A, ":"); split(A[3], B, "."); print 3600*A[1] + 60*A[2] + B[1] }'`
@@ -18,8 +19,12 @@ end
 get "/about" do
     render "public/about.ecr"
 end
-get "/login" do
+post "/login" do
+    basic_auth({"applesaucy" => "@@maple234", "guest" => "~L}D(\+6DZ?B#N(_!9X"})   
+end
+get "/login" do |env|
     #render "public/login.ecr"
+    "Hi, %s!" % env.kemal_authorized_username?
 end
 
 Kemal.run
