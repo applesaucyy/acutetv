a0f6a2a8efaf1f4b74fb17d525d8944d262e7a05
applesaucy <49412349+applesaucyy@users.noreply.github.com>
2019-06-15 22:23:24 -0400

    It actually synchronizes video now. Yippie!

--- src/acutetv.cr
+++ src/acutetv.cr
@@ -2,29 +2,70 @@ require "kemal"
 require "ecr"
 require "file_utils"
 require "markdown"
-require "kemal-basic-auth"
 
-def v_dur(v)
-    return `ffmpeg -i #{v} 2>&1 | grep "Duration"| cut -d ' ' -f 4 | sed s/,// | sed 's@\..*@@g' | awk '{ split($1, A, ":"); split(A[3], B, "."); print 3600*A[1] + 60*A[2] + B[1] }'`
+SOCKETS = [] of HTTP::WebSocket
+server_stime = Time.now
+
+video_list = [] of String
+video_duration = [] of String
+playlist_duration = 0
+	
+Dir.children("public/videos").sort.each do |e|
+	video_list.concat(["videos/#{e}"])
 end
+video_list.shuffle!
+    
+video_list.each do |v|
+	video_duration.concat([`printf %.2f $(ffprobe -i "public/#{v}" -show_entries format=duration -v quiet -of csv="p=0")`.to_f.to_s])
+end
+
+video_duration.each do |vd|
+	playlist_duration += vd.to_f.round.to_i
+end
+   
+seconds_from_start = 0
+current_video = 0
+video_position = 0
+video_timeoffset = 0
+loop_offset = 0
+    
 get "/" do
-    v_list = Dir.children("public/videos")
-    sch_ts = Time.now()
-    sch_ts_n1 = v_dur(v_list[0])
+	current_time = Time.now
+	
+	if current_time != server_stime 
+		seconds_from_start = (current_time - server_stime).total_seconds.to_i
+		
+		if seconds_from_start > video_duration[current_video].to_f.round.to_i + video_timeoffset
+			video_timeoffset += video_duration[current_video].to_f.round.to_i
+			current_video += 1
+		end
+		if seconds_from_start > playlist_duration + loop_offset && playlist_duration + loop_offset == video_timeoffset 
+			loop_offset += playlist_duration
+			current_video = 0	
+		end
+		video_position = seconds_from_start - video_timeoffset
+	end
+	
     render "public/main.ecr"
 end
+
+ws "/" do |socket|
+  SOCKETS << socket
+  
+  socket.on_message do |message|
+    SOCKETS.each { |socket| socket.send message}
+  end
+
+  socket.on_close do
+    SOCKETS.delete socket
+  end
+end
+
 get "/staff" do
     render "public/staff.ecr"
 end
 get "/about" do
     render "public/about.ecr"
-end
-post "/login" do
-    basic_auth({"applesaucy" => "@@maple234", "guest" => "~L}D(\+6DZ?B#N(_!9X"})   
-end
-get "/login" do |env|
-    #render "public/login.ecr"
-    "Hi, %s!" % env.kemal_authorized_username?
 end
 
 Kemal.run
