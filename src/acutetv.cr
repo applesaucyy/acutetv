4c9be806e01da0d5a6e962324fc72b8908f57b3b
applesaucy <49412349+applesaucyy@users.noreply.github.com>
2019-06-17 15:56:24 -0400

    Massive video player / sync fix
    I added Shaka player to handle adaptive streaming, so things run better now; i've joined the google botnet

--- src/acutetv.cr
+++ src/acutetv.cr
@@ -6,18 +6,18 @@ server_stime = Time.now
 video_list = [] of String
 video_duration = [] of String
 playlist_duration = 0
-	
+    
 Dir.children("public/videos").sort.each do |e|
-	video_list << "videos/#{e}"
+    video_list << "videos/#{e}"
 end
 
 video_list.shuffle!
 video_list.each do |v|
-	video_duration << `printf %.2f $(ffprobe -i "public/#{v}" -show_entries format=duration -v quiet -of csv="p=0")`.to_f.to_s
+    video_duration << `printf %.2f $(ffprobe -i "public/#{v}" -show_entries format=duration -v quiet -of csv="p=0")`.to_f.to_s
 end
 
 video_duration.each do |vd|
-	playlist_duration += vd.to_f.round.to_i
+    playlist_duration += vd.to_f.round.to_i
 end
    
 seconds_from_start = 0
@@ -27,22 +27,22 @@ video_timeoffset = 0
 loop_offset = 0
     
 get "/" do
-	current_time = Time.now
-	
-	if current_time != server_stime 
-		seconds_from_start = (current_time - server_stime).total_seconds.to_i
-		
-		if seconds_from_start > video_duration[current_video].to_f.round.to_i + video_timeoffset
-			video_timeoffset += video_duration[current_video].to_f.round.to_i
-			current_video += 1
-		end
-		if seconds_from_start > playlist_duration + loop_offset && playlist_duration + loop_offset == video_timeoffset 
-			loop_offset += playlist_duration
-			current_video = 0	
-		end
-		video_position = seconds_from_start - video_timeoffset
-	end
-	
+    current_time = Time.now
+    
+    if current_time != server_stime 
+        seconds_from_start = (current_time - server_stime).total_seconds.to_i
+        
+        if seconds_from_start > video_duration[current_video].to_f.round.to_i + video_timeoffset
+            video_timeoffset += video_duration[current_video].to_f.round.to_i
+            current_video += 1
+        end
+        if seconds_from_start > playlist_duration + loop_offset && playlist_duration + loop_offset == video_timeoffset 
+            loop_offset += playlist_duration
+            current_video = 0   
+        end
+        video_position = seconds_from_start - video_timeoffset
+    end
+    
     render "public/main.ecr"
 end
 
